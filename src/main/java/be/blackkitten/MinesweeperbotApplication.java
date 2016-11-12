75f23ed689878f46eb8ede99a34e809c8a62228f
BenBertrands <ben.bertrands@gmail.com>
2016-11-12 17:19:37 +0100

    spring start

---
+++ src/main/java/be/blackkitten/MinesweeperbotApplication.java
@@ -0,0 +1,12 @@
+package be.blackkitten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperbotApplication.class, args);
+	}
+}
